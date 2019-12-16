55ff675802eae43e31bf147db8555d44a2f87f74
ono5 <kuroneko_head_lord@yahoo.co.jp>
2019-12-17 06:11:42 +0900

    Card Component

--- monsters-rolodex/README.md
+++ monsters-rolodex/README.md
@@ -7,3 +7,6 @@ create-react-app monsters-rolodex
 
 ## Json Placeholder API
 [url](https://jsonplaceholder.typicode.com/users)
+
+## Monster
+[url](https://robohash.org/1?set=set2)
--- monsters-rolodex/src/components/card-list/cardlist.component.jsx
+++ monsters-rolodex/src/components/card-list/cardlist.component.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Card } from '../card/card.component'
 import './cardlist.style.css';
 
 export const CardList = props => {
     console.log(props)
-    return <div className='card-list'>{props.children}</div>;
+    return <div className='card-list'>
+        {props.monsters.map(monster => (
+            <Card key={monster.id} monster={monster} />
+        ))}  
+    </div>;
 }
