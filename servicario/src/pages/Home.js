6e376102f41bc473ae9793a4f9a0900d0b3a5715
ono5 <kuroneko_head_lord@yahoo.co.jp>
2020-02-05 20:34:42 +0900

    Added Hero Component

--- servicario/src/pages/Home.js
+++ servicario/src/pages/Home.js
@@ -3,6 +3,7 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import NavbarClone from '../components/NavbarClone'
+import Hero from '../components/Hero'
 import ServiceItem from '../components/service/ServiceItem'
 import { getServices } from 'store'
 
@@ -17,9 +18,9 @@ class Home extends React.Component {
     this.setState({services})
   }
 
-  renderServices = (services) => {
-    return services.map(service => <ServiceItem key={service.id} service={service} />)
-  }
+  renderServices = (services) => 
+    services.map(service => <ServiceItem key={service.id} service={service} />)
+  
 
   render() {
     const { services } = this.state
@@ -27,30 +28,8 @@ class Home extends React.Component {
       <div>
         <Navbar />
         <NavbarClone />
-        <section className="hero is-default is-bold">
-          <div className="hero-body">
-            <div className="container">
-              <div className="columns is-vcentered">
-                <div className="column is-5 is-offset-1 landing-caption">
-                  <h1 className="title is-1 is-bold is-spaced">
-                      Learn, Collaborate.
-                  </h1>
-                  <h2 className="subtitle is-5 is-muted">Lorem ipsum sit dolor amet is a dummy text used by typography industry </h2>
-                  <p>
-                    <a className="button cta rounded primary-btn raised">
-                        Get Started
-                    </a>
-                  </p>
-                </div>
-                <div className="column is-5 is-offset-1">
-                  <figure className="image is-4by3">
-                      <img src={process.env.PUBLIC_URL + '/worker.svg'} alt="Description" />
-                  </figure>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
+        <Hero />
+        
 
         <section className="section section-feature-grey is-medium">
           <div className="container">
