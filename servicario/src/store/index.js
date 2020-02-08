1e82fda5c8d0b2050fa4ee66cecfa8d691be3f35
ono5 <kuroneko_head_lord@yahoo.co.jp>
2020-02-09 06:42:23 +0900

    Added Logger

--- servicario/src/store/index.js
+++ servicario/src/store/index.js
@@ -1,6 +1,20 @@
 import { createStore, combineReducers } from 'redux'
 import servicesReducer from 'reducers'
 
+const addLoggerToDispatch = store => {
+  const dispatch = store.dispatch
+
+  return action => {
+    console.group(action.type)
+    console.log('%c prev state', 'color: gray', store.getState())
+    console.log('%c action', 'color: blue', action)
+    const returnValue = dispatch(action)
+    console.log('%c next state', 'color: green', store.getState())
+    console.groupEnd(action.type)
+    return returnValue
+  }
+}
+
 // actions | action creators
 // dispatch
 // reducers
@@ -14,6 +28,7 @@ const initStore = () => {
   const browserSupport = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   const store = createStore(serviceApp, browserSupport)
 
+  store.dispatch = addLoggerToDispatch(store)
   return store
 }
 
