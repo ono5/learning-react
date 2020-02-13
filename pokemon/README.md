28cdc7d10d0fc4b9e682ed20f0c5ba0d31abd38e
ono5 <kuroneko_head_lord@yahoo.co.jp>
2020-02-14 05:17:58 +0900

    Added Pokedex

---
+++ pokemon/README.md
@@ -0,0 +1,6 @@
+# Pokemon
+
+## URL
+
+[url1](https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/1.png)
+[url2](https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png)
---
+++ pokemon/src/Pokecard.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './Pokecard.css';
+
+const POKE_API = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/';
+
+class Pokecard extends Component {
+  render() {
+    let imgSrc = `${POKE_API}${this.props.id}.png`;
+    return (
+      <div className="Pokecard">
+        <h1>{this.props.name}</h1>
+        <img src={imgSrc} alt={this.props.name} />
+        <div>Type: {this.props.type}</div>
+        <div>Exp: {this.props.exp}</div>
+      </div>
+    );
+  }
+}
+
+export default Pokecard;
